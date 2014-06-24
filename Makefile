86dc5bb97695569fcdc4936423cff93bb1ca8ffd
Charlie Somerville <charlie@charliesomerville.com>
2014-06-24 23:21:41 +0200

    add stress test

--- Makefile
+++ Makefile
@@ -1,3 +1,25 @@
-CFLAGS = -Wall -Werror -pedantic -Wextra
+CFLAGS = -Wall -Werror -pedantic -Wextra -O3 -I vendor/hiredis
+
+TEST_CFLAGS = -iquote .
+
+.PHONY: all test clean
 
 all: thredis.o
+
+test: test/stress
+	test/stress
+
+clean:
+	rm -f test/stress thredis.o
+	git submodule foreach git clean -dfx
+
+thredis.o: vendor/hiredis/hiredis.h
+
+test/stress: test/stress.c thredis.o vendor/hiredis/libhiredis.a
+	$(CC) -o $@ $(CFLAGS) $(TEST_CFLAGS) $^
+
+vendor/hiredis/%: vendor/hiredis/Makefile
+	$(MAKE) -C vendor/hiredis $(subst vendor/hiredis/,,$@)
+
+vendor/hiredis/Makefile:
+	git submodule update --init
---
+++ test/stress.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#include "thredis.h"
+
+#define THREAD_COUNT 128
+
+#define FAIL(...) (fprintf(stderr, "\n"), fprintf(stderr, __VA_ARGS__), exit(1))
+
+struct thread_ctx {
+    pthread_t thread;
+    int number;
+    thredis_t* thredis;
+};
+
+void*
+thread_main(void* ctx_)
+{
+    struct thread_ctx* ctx = ctx_;
+    thredis_t* thredis = ctx->thredis;
+    int number = ctx->number;
+
+    for(int i = 0; i < 10000; i++) {
+        char str[16];
+        sprintf(str, "%d", i);
+
+        redisReply* reply = thredis_command(thredis, "ECHO %s", str);
+
+        if(!reply) {
+            FAIL("thredis_command returned NULL\n");
+        }
+
+        if(reply->type != REDIS_REPLY_STRING) {
+            FAIL("thredis_command did not return string\n");
+        }
+
+        if(strcmp(reply->str, str)) {
+            FAIL("thredis_command string reply was incorrect\n");
+        }
+
+        freeReplyObject(reply);
+
+        if((i % 100) == 0 && number == 0) {
+            fprintf(stderr, ".");
+        }
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    const char* redis_host = getenv("REDIS_HOST");
+    const char* redis_port = getenv("REDIS_PORT");
+
+    if(!redis_host) {
+        redis_host = "localhost";
+    }
+
+    if(!redis_port) {
+        redis_port = "6397";
+    }
+
+    redisContext* redis = redisConnect(redis_host, atoi(redis_port));
+    if(redis->err) {
+        FAIL("redisConnect failed: %s\n", redis->errstr);
+    }
+
+    thredis_t* thredis = thredis_new(redis);
+    if(!thredis) {
+        FAIL("thredis_new failed\n");
+    }
+
+    struct thread_ctx threads[THREAD_COUNT];
+
+    for(int i = 0; i < THREAD_COUNT; i++) {
+        threads[i].thredis = thredis;
+        threads[i].number = i;
+        if(pthread_create(&threads[i].thread, NULL, thread_main, &threads[i])) {
+            FAIL("pthread_create failed\n");
+        }
+    }
+
+    for(int i = 0; i < THREAD_COUNT; i++) {
+        if(pthread_join(threads[i].thread, NULL)) {
+            FAIL("pthread_join failed\n");
+        }
+    }
+
+    printf("\n");
+}
